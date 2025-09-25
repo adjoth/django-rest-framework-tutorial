a8b649113fef9a52444dc67ee236bde6e4314e17
Adam Thomas <thomasadam911@yahoo.com>
2025-09-25 15:10:27 -0500

    Reset state

--- tutorial/snippets/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from snippets import views
-
-urlpatterns = [
-    path('snippets/', views.snippet_list), # type: ignore
-    path('snippets/<int:pk>/', views.snippet_detail), # type: ignore
-]
